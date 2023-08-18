fd98c73c3cb70c4f764be74a8d7b2925620a8319
fsj0591 <994763265@qq.com>
2023-08-18 15:27:41 +0800

    user服务实体类

--- common/common-security/src/main/java/com/cb/common/security/pojo/User.java
+++ common/common-security/src/main/java/com/cb/common/security/pojo/User.java
@@ -1,37 +1,37 @@
-package com.cb.user.pojo.vo;
+package com.cb.common.security.pojo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 
 @Data
-public class UserLoginVO {
+@NoArgsConstructor
+public class User {
 
     /**
      * 主键
      */
-    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     /**
      * 用户名
      */
-    @TableField("name")
     private String name;
+
+    /**
+     * 密码,md5加密
+     */
+    private String password;
 
     /**
      * 邮箱
      */
-    @TableField("mail")
     private String mail;
 
     /**
      * 头像
      */
-    @TableField("image")
     private String image;
 
     /**
@@ -39,25 +39,21 @@ public class UserLoginVO {
      1 女
      2 未知
      */
-    @TableField("sex")
     private Boolean sex;
 
     /**
      * 是否身份认证
      */
-    @TableField("is_identity_authentication")
-    private Boolean identityAuthentication;
+    private Boolean identity;
 
     /**
      * 贵宾到期时间
      */
-    @TableField("vip")
     private Timestamp vip;
 
     /**
      * 注册时间
      */
-    @TableField("created_time")
     private Timestamp createdTime;
 
 }
